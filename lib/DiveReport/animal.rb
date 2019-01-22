7596a3a6841bd05401506461bb5767bb41120ee1
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-22 23:27:34 +0000

    refactor .scrape_directory_animals and create .print_names in animal class

--- lib/DiveReport/animal.rb
+++ lib/DiveReport/animal.rb
@@ -9,9 +9,14 @@ class Animal
     @name = name
     @url = url
     @@all << self
+    #binding.pry
   end
 
   def self.all
       @@all
   end
+
+  def self.print_names
+     self.all.each.with_index(1) {|animal, i| puts "#{i}. #{animal.name}"}
+   end
 end
--- lib/DiveReport/scraper.rb
+++ lib/DiveReport/scraper.rb
@@ -8,12 +8,23 @@ class Scraper
    html = open('http://www.divereport.com/site-directory/')
    doc = Nokogiri::HTML(open(html))
 
-   urls_array = doc.css("ul.sitemaplist li a")[0..30].map{|url| url.attr("href")}
-   names_array = doc.css("ul.sitemaplist li a")[0..30].map{|name| name.text}
-
-   Animal.new(name, url)
+   animals = doc.css("ul.sitemaplist li a")[0..30]
+   animals.each do |nodeset|
+     url = nodeset.attr("href")
+     name = nodeset.text
+     Animal.new(name, url)
+   end
   end
-end
+ end
+
+  # def self.scrape_animal_details(animal)
+  #   html = open(DIVE_REPORT_URL + animal.url)
+  #   doc = Nokogiri::HTML(html)
+  #   animal.img_link = (find css selector)
+  #   animal.locations = (find css selector - will be many locations)
+  # end
+
+
 
 
 
