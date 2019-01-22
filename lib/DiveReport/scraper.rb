6cdf49c8902ad05b10aa2430401ce4869eec56b4
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-22 20:58:05 +0000

    fixed syntax error

--- lib/DiveReport/scraper.rb
+++ lib/DiveReport/scraper.rb
@@ -1,20 +1,20 @@
 require 'pry'
 
-class DiveReport::Scraper
+class Scraper
   DIVE_REPORT_URL = "http://www.divereport.com/"
 
  def self.scrape_directory_animals
    html = open('http://www.divereport.com/site-directory/')
-   doc = Nokogiri::HTML(open(html)
+   doc = Nokogiri::HTML(open(html))
 
    doc.css("ul.sitemaplist li a")[0..30].each do |animal|
+     binding.pry
      name = doc.css("ul.sitemaplist li a").text
      url = doc.css("ul.sitemaplist li a").attr("href").value
      Animal.new(name, url)
-     binding.pry
- end
+   end
+  end
 end
-
 
 
 # animals = doc.css("ul.sitemaplist li a")[0..30]
