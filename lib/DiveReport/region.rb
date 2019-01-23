6cfa3cde7d61c94af3acd02640baab3ad98db4eb
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-23 04:31:58 +0000

    create initialize and .print_names

--- lib/DiveReport/region.rb
+++ lib/DiveReport/region.rb
@@ -1,3 +1,20 @@
-class DiveReport::Region
-  attr_accessor :name, :countries, :url, :dive_locations
+class Region
+  attr_accessor :name, :countries, :url
+
+  @@all = []
+
+  def initialize(name, url)
+    @name = name
+    @url = url
+    @@all << self
+  end
+
+  def self.all
+      @@all
+  end
+
+  def self.print_names
+     self.all.each.with_index(1) {|region, i| puts "#{i}. #{region.name}"}
+   end
+
 end
