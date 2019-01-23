44c60aba684c75b1523abf1482e482955500fa7a
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-23 02:55:09 +0000

    refactor to be DRY

--- lib/DiveReport/scraper.rb
+++ lib/DiveReport/scraper.rb
@@ -4,19 +4,6 @@ class Scraper
 
   DIVE_REPORT_URL = "http://www.divereport.com/"
 
- def self.scrape_directory_animals
-   html = open('http://www.divereport.com/site-directory/')
-   doc = Nokogiri::HTML(open(html))
-
-   animals = doc.css("ul.sitemaplist li a")[0..30]
-   animals.each do |nodeset|
-     url = nodeset.attr("href")
-     name = nodeset.text
-     Animal.new(name, url)
-   end
-  end
- end
-
   # def self.scrape_animal_details(animal)
   #   html = open(DIVE_REPORT_URL + animal.url)
   #   doc = Nokogiri::HTML(html)
@@ -25,9 +12,33 @@ class Scraper
   # end
 
 
+  def self.scrape_directory_site
+    html = open('http://www.divereport.com/site-directory/')
+    doc = Nokogiri::HTML(open(html))
+
+    objects = doc.css("ul.sitemaplist li a")
+    objects.each.with_index do |nodeset, i|
+      url = nodeset.attr("href")
+      name = nodeset.text
+      if i < 31
+        Animal.new(name, url)
+      # elsif objects.index < 44
+      #   Region.new(name, url)
+      # else
+      #   Country.new(name, url)
+      end
+    end
+   end
+  end
 
 
 
+    # def self.scrape_animal_details(animal)
+    #   html = open(DIVE_REPORT_URL + animal.url)
+    #   doc = Nokogiri::HTML(html)
+    #   animal.img_link = (find css selector)
+    #   animal.locations = (find css selector - will be many locations)
+    # end
 
 # animals = doc.css("ul.sitemaplist li a")[0..30]
 # regions = doc.css("ul.sitemaplist li a")[31..43]
