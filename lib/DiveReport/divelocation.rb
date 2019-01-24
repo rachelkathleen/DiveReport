2ffab7bb79082d687b06783a17f27f582d094d3c
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-24 23:18:54 +0000

    clarify find_by_url method

--- lib/DiveReport/divelocation.rb
+++ lib/DiveReport/divelocation.rb
@@ -15,7 +15,7 @@ class DiveLocation
   end
 
   def self.find_by_url(url)
-   self.all.detect {|country| country.url == url}.name
+   self.all.detect {|location| location.url == url}.name
  end
 
   def self.find_by_name(location_name)
