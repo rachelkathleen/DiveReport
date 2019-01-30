bea312e8e325803d97a9ae1f6542180402f76148
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-30 22:04:31 +0000

    add .add_location_details

--- lib/DiveReport/divelocation.rb
+++ lib/DiveReport/divelocation.rb
@@ -1,6 +1,5 @@
-require 'pry'
 class DiveLocation
-  attr_accessor :name, :url, :country, :description, :visibility, :depth_range, :water_temp, :area, :animals
+  attr_accessor :name, :url, :country, :description, :visibility, :depth_range, :water_temp, :area
 
   @@all = []
 
@@ -14,9 +13,18 @@ class DiveLocation
       @@all
   end
 
+  def self.add_location_attributes(dive_location)
+    dive_location_hash = Scraper.scrape_dive_location_details(dive_location)
+    dive_location.description = dive_location_hash[:description]
+    dive_location.visibility = dive_location_hash[:visibility]
+    dive_location.depth_range = dive_location_hash[:depth_range]
+    dive_location.water_temp = dive_location_hash[:water_temp]
+    dive_location.area = dive_location_hash[:area]
+  end
+
   def self.find_by_url(url)
    self.all.detect {|location| location.url == url}
- end
+  end
 
   def self.find_by_name(location_name)
     self.all.find {|location| location.name == location_name}
